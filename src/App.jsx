9e1236a03cdf44383e88e0f7f74fdec03645b757
JanLascala <lascala.jan@gmail.com>
2025-03-28 15:44:11 +0100

    fetch posts from API on component mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import { useState, useEffect } from 'react'
 
+const api_posts = 'http://localhost:3000/api/v1/posts';
+
 function App() {
+
+  useEffect(() => {
+    fetchdata(api_posts);
+  }, [])
+
+  fetch(api_posts)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
 
 }
 
