02f92731b39a9b8d9c2e5e72f2e5abebe8d07e9a
JanLascala <lascala.jan@gmail.com>
2025-03-28 17:23:39 +0100

    implement data fetching in useEffect and add basic markup structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ const api_posts = 'http://localhost:3000/api/v1/posts';
 
 function App() {
 
+  //this is to not repeat ad infinitum
   useEffect(() => {
     fetchdata(api_posts);
   }, [])
-
+  //here i fetch the data 
   function fetchdata(url = api_posts) {
     console.log('this is data from bakery', url);
 
@@ -18,6 +19,24 @@ function App() {
       })
       .catch(err => console.error('Error fetching data:', err));
   }
+  return (
+    //markup
+    <>
+      <h1>Ricette tradizionali</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h3>title</h3>
+            <img src="" alt="" /><span>here the img</span>
+            <div>
+              <p>description</p>
+              <p>tags</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 
 
 }
